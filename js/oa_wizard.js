53b6775621c10afd647fea7fa3dab9f1cf03d6ca
Micah Godbolt <mgodbolt@phase2technology.com>
2014-08-29 13:52:17 -0700

    Basic wizard functionality and layout

--- js/oa_wizard.js
+++ js/oa_wizard.js
@@ -14,8 +14,160 @@
         'help' : The help text for this step
         'showhelp' : TRUE to show help text, false to hide help text
        */
+      var   $form = $('.node-form');
+            $html = "";
+
+////
+// Set up HTML Stubs
+////
+
+            // Set up the Nav Container HTML
+            $navItems = $("<ul/>", {
+                        "class" : "nav",
+                        "role"  : "tablist",
+                        html    : $("<li/>", {
+                                    html: $("<a/>", {
+                                            "class" : "toggle-nav",
+                                            "href" : "#",
+                                            click : function(e) {
+                                              e.preventDefault();
+                                              $('#oa-wizard').toggleClass('open');
+                                            }
+                                          }).append(
+                                            $("<span/>", {
+                                              "class" : "glyphicon glyphicon-th-list",
+                                            })
+                                          )
+                                  })
+                      });
+
+            // Set Up the Tab Container HTML
+            $tabContent = $("<div/>", {
+                         "class" : "tab-content"
+                         });
+
+////
+// Step Through $steps array
+////
+
+      $.each($steps, function(key,value) {
+        var tab = "",
+            fields = value['fields'],
+            prevState = (key == 0 ? "disabled" : "enabled"),
+            nextState = (key == Object.keys($steps).length -1 ? "disabled" : "enabled");
+
+        // clean button container
+        var $buttons = $("<div/>", {
+                        "class" : "buttons"
+                      });
+
+        // clean tab container
+        $tab = $("<div/>", {
+                  "class" : "tab-pane",
+                  "id"    : "tab" + (key + 1),
+                });
+
+
+////
+// Append all Navigation Items
+////
+        $("<li/>", {
+          html : $("<a/>", {
+                  "href"        : "#tab" + (key + 1),
+                  "role"        : "tab",
+                  "data-toggle" : "tab",
+                  text          : value['title']
+                  }).append(
+                    $("<span/>", {
+                      "class" : "badge",
+                      text    : key + 1
+                    }
+                  ))
+        }).appendTo($navItems);
+
+
+////
+// Append Fields and buttons to tab content
+////
+
+        $.each(fields, function(i, field) {
+          field = field.replace(/[_]/g, "-");
+          if (field == "title") {
+            $(".form-item-title").detach().appendTo($tab);
+          }
+          else {
+            $('.field-name-' + field).detach().appendTo($tab);
+          }
+        });
+
+        // create prev/next buttons
+        $prevButton = $("<button/>", {
+                        "class" : "btn btn-primary",
+                        "type" : "button",
+                        href : "#",
+                        text : "Previous",
+                        click : function(e) {
+                          e.preventDefault;
+                          $('#oa-wizard [data-toggle="tab"]').eq(key - 1).tab('show');
+                        }
+                      }).attr(prevState, "");
+
+        $nextButton = $("<button/>", {
+                        "class" : "btn btn-primary",
+                        "type" : "button",
+                        href : "#",
+                        text : "Next",
+                        click : function(e) {
+                          e.preventDefault;
+                          $('#oa-wizard [data-toggle="tab"]').eq(key + 1).tab('show');
+                        }
+                      }).attr(nextState, "");
+
+        // Append Buttons
+        $buttons.append($prevButton).append($nextButton);
+
+        if (nextState == 'disabled') {
+          $('#edit-actions').detach().appendTo($buttons);
+        };
+
+        // place buttons at bottom of tab
+        $buttons.appendTo($tab);
+
+        // Add tab to tabContent container
+        $tab.appendTo($tabContent);
+
+      });
+
+
+////
+// Put it all together
+////
+
+      $html = $("<div/>", {
+                "class" : "",
+                "id"    : "oa-wizard",
+                html    : $("<div/>", {
+                            "class" : "row",
+                            html    : $navItems
+                          }).append(
+                            $('<div/>', {
+                                "class" : "content",
+                                html    : $tabContent
+                            }))
+              });
+
+
+////
+// Hide original form, insert new wizard and show 1st tab
+////
+
+      $form.find('div').hide();
+      $form.prepend($html);
+      // show first item
+      $('#oa-wizard [data-toggle="tab"]').eq(0).tab('show');
       console.log($steps);
     }
   }
 
 }(jQuery));
+
